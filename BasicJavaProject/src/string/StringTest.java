453974052ad994fc1ee2edf3713999ae5f6d352d
ItBoyPjq <495461094@qq.com>
2019-10-06 15:53:46 +0800

    第一次测试

---
+++ BasicJavaProject/src/string/StringTest.java
@@ -0,0 +1,12 @@
+package string;
+
+/**
+ * @Author Pjq
+ * @Date 2019/10/6
+ **/
+public class StringTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
